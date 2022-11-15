490a63083837798d80cb562d2a4bcdfa353524fd
Moyses Thomaz <moyses.th@gmail.com>
2022-11-14 21:30:41 -0300

    adjusting

--- src/app/paginas/tabelas/tabelas.component.ts
+++ src/app/paginas/tabelas/tabelas.component.ts
@@ -9,10 +9,10 @@ export class TabelasComponent implements OnInit {
 
   constructor() { }
 
-  public produtos:any[] = []
+  public produtos: any[] = []
 
   ngOnInit(): void {
-    
+
     this.produtos.push(
       {
         id: 1,
@@ -26,12 +26,37 @@ export class TabelasComponent implements OnInit {
       {
         id: 2,
         nome: 'Maça',
-        descricao: 'Uma Ma;ca Pera',
+        descricao: 'Uma maça pera',
         preco: 8.5
       }
     )
-    
 
+    this.produtos.push(
+      {
+        id: 3,
+        nome: 'Melão',
+        descricao: 'Um melão amarelo',
+        preco: 6.50
+      }
+    )
+
+    this.produtos.push(
+      {
+        id: 4,
+        nome: 'Romã',
+        descricao: 'Um romã',
+        preco: 15.5
+      }
+    )
+
+    this.produtos.push(
+      {
+        id: 5,
+        nome: 'Melância',
+        descricao: 'Uma melância',
+        preco: 11.5
+      }
+    )
   }
 
 }
